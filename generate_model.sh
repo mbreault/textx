ec5502e4790669fa6707c277778784f13813ea2e
Mike Breault <mbreault@users.noreply.github.com>
2022-10-08 08:29:15 -0500

    initial commit

---
+++ generate_model.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+textx generate hello.tx --target dot
---
+++ hello.py
@@ -0,0 +1,6 @@
+from textx import metamodel_from_file
+hello_meta = metamodel_from_file('hello.tx')
+hello_model = hello_meta.model_from_file('example.hello')
+
+print("Greeting", ", ".join([to_greet.name
+                            for to_greet in hello_model.to_greet]))
